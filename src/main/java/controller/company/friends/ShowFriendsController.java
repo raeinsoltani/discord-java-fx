c28f19d9b6a6afc002f2bf6a257f000cdfb15842
Raein Soltani <raeen.soltani@yahoo.com>
2022-07-12 17:01:06 +0430

    feat: added block function

--- src/main/java/controller/company/friends/ShowFriendsController.java
+++ src/main/java/controller/company/friends/ShowFriendsController.java
@@ -1,9 +1,13 @@
 package controller.company.friends;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import org.ce.ap.discord.client.business.CommandParser;
 import org.ce.ap.discord.common.entity.business.Person;
+
+import static org.ce.ap.discord.common.boot.Bootstrapper.LOGGER;
 
 public class ShowFriendsController {
 
@@ -27,6 +31,17 @@ public class ShowFriendsController {
 //        statusText.setText();
         phoneNumberText.setText(person.getPhoneNumber());
 //        emailText.setText(person.getEmail());
+    }
+
+    @FXML
+    void block(ActionEvent event) {
+        LOGGER.info("Blocking");
+        Object response = CommandParser.networkService.block(CommandParser.loginUser.getId(), usernameText.getText());
+        if (response != null)
+            LOGGER.error("Blocking failed because {}", String.valueOf(response));
+        else
+            LOGGER.info("Blocked : {}", usernameText.getText());
+
     }
 
 }
